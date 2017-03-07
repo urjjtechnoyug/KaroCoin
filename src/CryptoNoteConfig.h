a2b5fd0761acdd3e4e5168d22ec9c589cc287087
urjjtechnoyug <urjjtechnoyug@gmail.com>
2017-03-07 22:40:05 +0530

    Add files via upload

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "grocoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101a8a027d167dcc0b81a17ef8a6b6865dd76ef3441c60c6a0d2a39fa22edf1ebb9";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "104.236.101.182:17236"
-  //"your_seed_ip2.com:8080",
+  "104.236.101.182:17236",
+  "138.197.46.250:17236",
 };
 
 struct CheckpointData {
